4afb3a58773b227261c94bc22727a2e77b75092f
Tjcarroll1 <tjcarroll1@icloud.com>
2024-11-07 08:21:25 +1000

    changed spinner

--- src/components/LoadingSpinner.tsx
+++ src/components/LoadingSpinner.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 
 const LoadingSpinner: React.FC = () => {
   return (
-    <div className="fixed inset-0 flex items-center justify-center bg-gray-800 bg-opacity-50">
-      <div className="animate-spin rounded-full h-32 w-32 border-t-2 border-b-2 border-brand-green"></div>
+    <div className="fixed inset-0 flex items-center justify-center bg-zinc-900/50">
+      <div className="animate-spin rounded-full h-32 w-32 border-t-[3px] border-b-[3px] border-t-[#0ff] border-b-[#f0f]"></div>
     </div>
   );
 };
