e5d257f0208b3f0d89b57f6163f2993845b1be95
Tjcarroll1 <tjcarroll1@icloud.com>
2024-11-06 17:51:01 +1000

    config2

--- next.config.ts
+++ next.config.ts
@@ -3,22 +3,29 @@ const nextConfig = {
   async headers() {
     return [
       {
-        source: '/:path*',
-        headers: [
-          {
-            key: 'X-Frame-Options',
-            value: 'SAMEORIGIN'
-          }
-        ]
-      },
-      {
         source: '/widget.js',
         headers: [
           {
             key: 'Access-Control-Allow-Origin',
             value: '*'
+          },
+          {
+            key: 'X-Frame-Options',
+            value: 'ALLOW-FROM *'
+          },
+          {
+            key: 'Content-Type',
+            value: 'application/javascript'
           }
         ]
+      }
+    ]
+  },
+  async rewrites() {
+    return [
+      {
+        source: '/widget.js',
+        destination: '/public/widget.js'
       }
     ]
   }
